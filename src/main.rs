fb3c49e9826eae0995f254e794cb7dde5e5e1160
Nathan Agbomedarho <nathanagbomed@proton.me>
2024-09-17 00:31:02 -0400

    Implement parsing torrent file.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use serde_json;
-use std::env;
-use serde_bencode::de;
-use serde_bencode::ser;
+use std::{env, fs};
+use serde_bencode::{de, ser, value::Value};
 
-//using serde_bencode
+fn decode_torrent_file(file_path: &str) -> Value {
+
+    let torrent_content = fs::read(file_path).expect("Failed to read torrent file");
+    de::from_bytes(&torrent_content).expect("Failed to decode torrent file")
+}
+
+
 fn decode_bencoded_value(encoded_value: &[u8]) -> serde_json::Value {
 
     let decoded: serde_bencode::value::Value = de::from_bytes(encoded_value)
         .expect("Failed to decode bencoded value");
 
     match decoded {
-
         serde_bencode::value::Value::Int(i) => serde_json::Value::Number(serde_json::Number::from(i)),
         serde_bencode::value::Value::Bytes(b) => {
             let s = String::from_utf8_lossy(&b);
@@ -48,6 +52,26 @@ fn main() {
         let encoded_value = &args[2].as_bytes(); // Convert String to &[u8]
         let decoded_value = decode_bencoded_value(encoded_value);
         println!("{}", decoded_value.to_string());
+    } 
+    else if command == "info" {
+        let torrent_file = &args[2];
+        let decoded_value = decode_torrent_file(torrent_file);
+
+        if let Value::Dict(torrent_dict) = decoded_value {
+
+            //Extract tracker URL
+            if let Some(Value::Bytes(announce)) = torrent_dict.get("announce".as_bytes()) {
+                let tracker_url = String::from_utf8_lossy(announce);
+                println!("Tracker URL: {}", tracker_url);
+            }
+
+            //Extract file length
+            if let Some (Value::Dict(info)) = torrent_dict.get("info".as_bytes()) {
+                if let Some(Value::Int(length)) = info.get("length".as_bytes()) {
+                    println!("Length: {}", length);
+                }
+            }
+        }
     } else {
         println!("unknown command: {}", args[1])
     }
