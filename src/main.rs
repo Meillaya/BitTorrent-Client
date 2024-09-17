429123267bcbdcab8d2dc6f03f4bbc0969b612cf
Nathan Agbomedarho <nathanagbomed@proton.me>
2024-09-17 00:10:54 -0400

    Implement decoding lists and dictionaries.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use serde_json;
 use std::env;
 use serde_bencode::de;
+use serde_bencode::ser;
 
 //using serde_bencode
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+fn decode_bencoded_value(encoded_value: &[u8]) -> serde_json::Value {
 
-    let decoded: serde_bencode::value::Value = de::from_bytes(encoded_value.as_bytes())
+    let decoded: serde_bencode::value::Value = de::from_bytes(encoded_value)
         .expect("Failed to decode bencoded value");
 
     match decoded {
@@ -15,33 +16,28 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
             let s = String::from_utf8_lossy(&b);
             serde_json::Value::String(s.into_owned())
         },
-        _ => panic!("Unsopported bencoded type"),
+        serde_bencode::value::Value::List(l) => {
+            let json_list: Vec<serde_json::Value> = l.into_iter()
+                .map(|v|{
+                    let encoded = ser::to_bytes(&v).expect("Failed to encode value");
+                    decode_bencoded_value(&encoded)
+                })
+                .collect();
+            serde_json::Value::Array(json_list)
+        },
+        serde_bencode::value::Value::Dict(d) => {
+            let json_map: serde_json::Map<String, serde_json::Value> = d.into_iter()
+            .map(|(k, v)| {
+                let key = String::from_utf8_lossy(&k).into_owned();
+                let encoded = ser::to_bytes(&v).expect("Failed to encode value");
+                let value = decode_bencoded_value(&encoded);
+                (key,value)
+            })
+            .collect();
+        serde_json::Value::Object(json_map)
+        }
     }
-
 }
-
-//Using standard libraries
-// #[allow(dead_code)]
-// fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-//     if encoded_value.starts_with('i') && encoded_value.ends_with('e') {
-
-
-//         let number_str = &encoded_value[1..encoded_value.len() -1];
-//         match number_str.parse::<i64>(){
-//             Ok(number) => serde_json::Value::Number(serde_json::Number::from(number)),
-//             Err(_) => panic!("Invalid bencoded integer: {}", encoded_value),
-//         }
-//     }
-//     else if encoded_value.chars().next().unwrap().is_digit(10) {
-//         let colon_index = encoded_value.find(':').unwrap();
-//         let number_string = &encoded_value[..colon_index];
-//         let number = number_string.parse::<i64>().unwrap();
-//         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-//         return serde_json::Value::String(string.to_string());
-//     } else {
-//         panic!("Unhandled encoded value: {}", encoded_value)
-//     }
-// }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
@@ -49,10 +45,7 @@ fn main() {
     let command = &args[1];
 
     if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        // println!("Logs from your program will appear here!");
-
-        let encoded_value = &args[2];
+        let encoded_value = &args[2].as_bytes(); // Convert String to &[u8]
         let decoded_value = decode_bencoded_value(encoded_value);
         println!("{}", decoded_value.to_string());
     } else {
