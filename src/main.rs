3c01fcd03f53eddaf3cfda5eacfbb5783f9378d1
Nathan Agbomedarho <nathanagbomed@proton.me>
2024-09-24 23:43:37 -0400

    some progress

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-use hex;
 use reqwest;
 use serde_json;
 use std::{env, fs, fmt};
 use serde_bencode::{de, ser, value::Value};
 use serde_urlencoded;
 use sha1::{Sha1, Digest};
-use std::collections::HashMap;
 use reqwest::blocking::Client;
-use std::net::{Ipv4Addr, SocketAddrV4};
-
+use std::net::Ipv4Addr;
+use percent_encoding::{percent_encode, NON_ALPHANUMERIC};
 
 #[derive(Debug)]
 enum TorrentError {
@@ -17,6 +15,7 @@ enum TorrentError {
     MissingKey(&'static str),
     UnexpectedType(&'static str),
     ReqwestError(reqwest::Error),
+    UrlEncodingError(serde_urlencoded::ser::Error),
 }
 
 impl fmt::Display for TorrentError {
@@ -27,6 +26,7 @@ impl fmt::Display for TorrentError {
             TorrentError::MissingKey(key) => write!(f, "Missing Key: {}", key),
             TorrentError::UnexpectedType(key) => write!(f, "Unexpected Error: {}", key),
             TorrentError::ReqwestError(e) => write!(f, "Reqwest: {}", e),
+            TorrentError::UrlEncodingError(key) => write!(f, "UrlEncodingError: {}", key),
         }
     }
 }
@@ -43,6 +43,17 @@ impl From <serde_bencode::Error> for TorrentError {
     }
 }
 
+impl From<reqwest::Error> for TorrentError {
+    fn from(error: reqwest::Error) -> Self {
+        TorrentError::ReqwestError(error)
+    }
+}
+
+impl From<serde_urlencoded::ser::Error> for TorrentError {
+    fn from(error: serde_urlencoded::ser::Error) -> Self {
+        TorrentError::UrlEncodingError(error)
+    }
+}
 fn decode_torrent_file(file_path: &str) -> Result<Value, TorrentError> {
 
     let torrent_content = fs::read(file_path)?;
@@ -83,81 +94,88 @@ fn decode_bencoded_value(encoded_value: &[u8]) -> Result<serde_json::Value, Torr
     })
 }
 
-fn extract_torrent_info(decoded_value: Value) -> Result<(), TorrentError> {
 
-    if let Value::Dict(torrent_dict) = decoded_value {
+fn get_peers(torrent_file: &str) -> Result<(), TorrentError> {
+    let decoded_value = decode_torrent_file(torrent_file)?;
+    let (tracker_url, info_hash, length) = extract_torrent_info(decoded_value)?;
 
-        let announce = torrent_dict.get("announce".as_bytes())
-            .ok_or(TorrentError::MissingKey("announce"))?;
-        if let Value::Bytes(announce_bytes) = announce {
-            let tracker_url = String::from_utf8_lossy(announce_bytes);
-            println!("Tracker URL: {}", tracker_url);
-        }
-        else {
-            return Err(TorrentError::UnexpectedType("announce"));
+    
+    let encoded_info_hash = percent_encode(&info_hash, NON_ALPHANUMERIC).to_string();
 
-        }
+    let params = [
+        ("info_hash", encoded_info_hash),
+        ("peer_id", "00112233445566778899".to_string()),
+        ("port", "6881".to_string()),
+        ("uploaded", "0".to_string()),
+        ("downloaded", "0".to_string()),
+        ("left", length.to_string()),
+        ("compact", "1".to_string()),
+    ];
 
-        let info = torrent_dict.get("info".as_bytes())
-        .ok_or(TorrentError::MissingKey("info"))?;
-        if let Value::Dict(info_dict) = info {
-            let length = info_dict.get("length".as_bytes())
-                .ok_or(TorrentError::MissingKey("length"))?;
-            if let Value::Int(length_value) = length {
-                println!("Length: {}", length_value);
-            } else {
-                return Err(TorrentError::UnexpectedType("length"));
-            }
+    let client = Client::new();
+    let url = format!("{}?{}", tracker_url, serde_urlencoded::to_string(&params)?);
+    
+    let response = client.get(&url).send()?;
+    let response_bytes = response.bytes()?;
+    let response_value: Value = de::from_bytes(&response_bytes)?;
 
-            let info_hash = calculate_info_hash(info)?;
-            println!("Info Hash: {}", info_hash);
-
-            let piece_length = info_dict.get("piece length".as_bytes())
-                .ok_or(TorrentError::MissingKey("piece length"))?;
-            if let Value::Int(piece_length_value) = piece_length {
-                println!("Piece Length: {}", piece_length_value);
-            } else {
-                return Err(TorrentError::UnexpectedType("piece length"));
-            }
-
-
-            let pieces = info_dict.get("pieces".as_bytes())
-                .ok_or(TorrentError::MissingKey("pieces"))?;
-            if let Value::Bytes(pieces_bytes) = pieces {
-
-                println!("Pieces Hashes:");
-                for chunk in pieces_bytes.chunks(20) {
-                    println!("{}", hex::encode(chunk));
+    if let Value::Dict(response_dict) = response_value {
+        if let Some(Value::Bytes(peers_bytes)) = response_dict.get(b"peers" as &[u8]) {
+            for chunk in peers_bytes.chunks(6) {
+                if chunk.len() == 6 {
+                    let ip = Ipv4Addr::new(chunk[0], chunk[1], chunk[2], chunk[3]);
+                    let port = u16::from_be_bytes([chunk[4], chunk[5]]);
+                    println!("{}:{}", ip, port);
                 }
-            } else {         
-                return Err(TorrentError::UnexpectedType("pieces"));
+            }
+        } else {
+            println!("No peers found in the response");
+        }
+    } else {
+        return Err(TorrentError::UnexpectedType("tracker response"));
+    }
+    Ok(())
+}
+
+fn extract_torrent_info(decoded_value: Value) -> Result<(String, Vec<u8>, i64), TorrentError> {
+    if let Value::Dict(torrent_dict) = decoded_value {
+        let announce = torrent_dict.get(b"announce" as &[u8])
+            .ok_or(TorrentError::MissingKey("announce"))?;
+        let tracker_url = if let Value::Bytes(announce_bytes) = announce {
+            String::from_utf8_lossy(announce_bytes).to_string()
+        } else {
+            return Err(TorrentError::UnexpectedType("announce"));
+        };
+
+        let info = torrent_dict.get(b"info" as &[u8])
+            .ok_or(TorrentError::MissingKey("info"))?;
+        let info_hash = if let Value::Dict(info_dict) = info {
+            let encoded = ser::to_bytes(info)?;
+            let mut hasher = Sha1::new();
+            hasher.update(&encoded);
+            hasher.finalize().to_vec()
+        } else {
+            return Err(TorrentError::UnexpectedType("info"));
+        };
+        
+        let length = if let Value::Dict(info_dict) = info {
+            if let Some(Value::Int(length)) = info_dict.get(b"length" as &[u8]) {
+                *length
+            } else {
+                return Err(TorrentError::MissingKey("length"));
             }
         } else {
             return Err(TorrentError::UnexpectedType("info"));
-        }
+        };
+
+        Ok((tracker_url, info_hash, length))
     } else {
-        return Err(TorrentError::UnexpectedType("torrent"));
+        Err(TorrentError::UnexpectedType("torrent"))
     }
-    Ok(())
-
 }
 
-fn calculate_info_hash(info: &Value) -> Result<String, TorrentError> {
-
-    let encoded = ser::to_bytes(info)?;
-    let mut hasher = Sha1::new();
-    hasher.update(&encoded);
-    let result = hasher.finalize();
-    Ok(hex::encode(result))
-}
-
-fn discover_peers(torrent_file: &str) -> Result<(), TorrentError> {
-
-    
 
 
-    
-}
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() -> Result<(), TorrentError> {
 
@@ -172,13 +190,19 @@ fn main() -> Result<(), TorrentError> {
             .as_bytes();
             let decoded_value = decode_bencoded_value(encoded_value)?;
             println!("{}", decoded_value.to_string());
-        }
+        },
         "info" => {
-
             let torrent_file = args.get(2).ok_or(TorrentError::MissingKey("torrent file"))?;
             let decoded_value = decode_torrent_file(torrent_file)?;
-            extract_torrent_info(decoded_value)?;
-        }
+            let (tracker_url, info_hash, length) = extract_torrent_info(decoded_value)?;
+            println!("Tracker URL: {}", tracker_url);
+            println!("Length: {}", length);
+            println!("Info Hash: {}", hex::encode(info_hash));
+        },
+        "peers" => {
+            let torrent_file = args.get(2).ok_or(TorrentError::MissingKey("torrent file"))?;
+            get_peers(torrent_file)?;
+        },
         _ => println!("Unknown command: {}", command),
     }
 
